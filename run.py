2d5da4bbfe3a2dc6020fee971b45a6589989b527
isados <isadosry@gmail.com>
2022-04-26 00:40:37 +0300

    changed opps sheet and also included NPS sheet

--- run.py
+++ run.py
@@ -1,18 +1,26 @@
 #!/usr/bin/env python3
 
 import os
+import json
 import tempfile
+from pprint import pprint
+from pytest import param
+import requests
+import asyncio
+import aiohttp
+import time
 import numerics
 from numerics import months
 import opp
 import pygsheets
+import numpy as np
 import pandas as pd
 from functools import partial
 from utils import write_base64str_obj_to_file
 from datetime import datetime
 
 # numbers, covid, opps
-RUN_THESE = ['opps', 'covid', 'numbers']
+RUN_THESE = ['opps', 'covid', 'numbers', 'nps']
 def main():
     # Get credentials from service-account-file to access Google Sheets
     print("Creating temporary file for service account credentials...")
@@ -27,6 +35,7 @@ def main():
     workbook = gc.open_by_key(os.environ["SPREADSHEET_ID"])
     # Create handy function to write to sheets
     set_worksheet_todf = partial(pygsheets.Worksheet.set_dataframe, start="A1", copy_head=True)
+    set_opps_worksheet_todf = partial(pygsheets.Worksheet.set_dataframe, start="B5", copy_head=True)
     set_list_of_backgrnds_todf = partial(pygsheets.Worksheet.set_dataframe, start="E2", copy_head=False)
 
     if 'covid' in RUN_THESE:
@@ -55,9 +64,8 @@ def main():
         opp_df, backgrnds_df = opp.get()
         opp_sheet = workbook.worksheet_by_title(os.environ["OppsSheet"])
         backgrounds_sheet = workbook.worksheet_by_title(os.environ["ListofThings"])
-        opp_sheet.clear()
         ### Push it to Google Sheets
-        set_worksheet_todf(opp_sheet, opp_df)
+        set_opps_worksheet_todf(opp_sheet, opp_df)
         set_list_of_backgrnds_todf(backgrounds_sheet, backgrnds_df)
 
     if 'numbers' in RUN_THESE:
@@ -81,6 +89,95 @@ def main():
         perf_sheet.clear()
         ### Push it to Google Sheets
         set_worksheet_todf(perf_sheet, whole_df)
+    if 'nps' in RUN_THESE:
+
+        dates = [{'name':'Fruit', 'value': ('2022-01-01', '2022-06-30')},
+                {'name': 'Fish', 'value': ('2022-07-01', '2022-12-31')}
+            ]
+        
+        programs = [
+            {'name': 'GTe', 'id': 9},
+            {'name': 'GTa', 'id': 8},
+            {'name': 'GV', 'id': 7},
+        ]
+        entity_types = [
+            {'dept_prefix': 'i', 'value': 'opportunity'},
+            {'dept_prefix': 'o', 'value': 'person'},
+            ]
+
+        with open('mea_countries.json') as file:
+            countries = json.load(file)['relevant']
+        # params = {
+        #     'access_token': os.environ['ACCESS_TOKEN'],
+        #     'start_date': '2020-12-01',
+        #     'end_date': '2022-03-31',
+        #     'entity_type': 'opportunity',
+        #     'programmes[]': 7,
+        #     'entity_id': 1609
+        # }
+        async def _get_nps(params):
+            async with aiohttp.ClientSession() as session:
+                async with session.get('https://analytics.api.aiesec.org/v2/nps/analytics.json', params=params) as resp:
+                    res = await resp.json()
+            res = res['analytics']
+            try:
+                nps = (res['total_promoters']['doc_count'] - res['total_detractors']['doc_count'])/res['total_responses']['doc_count']*100
+            except ZeroDivisionError:
+                nps = 0
+
+                # NPS (float), NPS (Int), # of Responses
+            return nps, int(round(nps)), res['total_responses']['doc_count']
+
+        async def _get_response_asrow(params, peak, mc, dept_prefix, program):
+            nps_ft, nps_int, num_responses = await _get_nps(params)
+            return [
+                peak, mc, dept_prefix+program, nps_ft, nps_int, num_responses
+            ]
+
+
+        async def get_nps():
+            count = 0
+            funcs = []
+            for peak in dates:
+                for e_typ in entity_types:
+                    for prog in programs:
+                        for mc in countries:
+                            params = {
+                                'access_token': os.environ['ACCESS_TOKEN'],
+                                'start_date': peak['value'][0],
+                                'end_date': peak['value'][1],
+                                'entity_type': e_typ['value'],
+                                'programmes[]': prog['id'],
+                                'entity_id': mc['id']
+                            }
+                            count+=1
+                            # Peak 2022, MC, dept, NPS Score
+                            funcs.append(_get_response_asrow(params,
+                                                        peak['name'],
+                                                        mc['name'],
+                                                        e_typ['dept_prefix'],
+                                                        prog['name']))
+            print(f"Processing requests...")
+            nps_data = await asyncio.gather(
+                *funcs
+            )
+            nps_data_array = np.array(nps_data)
+            nps_df = pd.DataFrame(nps_data_array, columns=['Peak',
+                                                    'MC',
+                                                    'Program',
+                                                    'NPS Score',
+                                                    'NPS Score (Rounded)',
+                                                    '# of Responses'])
+            return nps_df
+        start = time.perf_counter()
+        nps_df = asyncio.run(get_nps())
+        print(nps_df)
+        nps_sheet = workbook.worksheet_by_title(os.environ["NPSSheet"])
+        nps_sheet.clear()
+        ### Push it to Google Sheets
+        set_worksheet_todf(nps_sheet, nps_df)
+        print(f'Time Taken for all NPS requests: {time.perf_counter()-start:0.4f} seconds')
+
 
     status_sheet = workbook.worksheet_by_title(os.environ["StatusUpdateSheet"])
     status_sheet.clear()
